3d8788dd3dad8345b6f1a30830ad7c6584387c2d
Roy Reyes <roy.reyes@itexico.com>
2020-04-15 20:12:33 -0500

    No Permissions needed

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Peer from 'peerjs';
+import { createEmptyVideoTrack, createEmptyAudioTrack } from './utils'
 
 export function Viewer() {
   const peer = new Peer('234567', {
@@ -7,13 +8,14 @@ export function Viewer() {
     port: 9000,
     path: '/myapp'
   });
-  navigator.mediaDevices.getDisplayMedia({ video: true }).then(_ => {
-    const call = peer.call('123456', _);
-    call.on('stream', stream => {
-      const video = document.querySelector('video');
-      video.srcObject = stream;
-    })
-  })
+  const audioTrack = createEmptyAudioTrack();
+  const videoTrack = createEmptyVideoTrack({ width: 640, height: 480 });
+  const mediaStream = new MediaStream([audioTrack, videoTrack]);
+  const call = peer.call('123456', mediaStream);
+  call.on('stream', stream => {
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+  });
   return (
     <div className="App">
       <video controls autoPlay playsInline muted={false} volume={0} style={{ width: 1000, height: 600 }} />
